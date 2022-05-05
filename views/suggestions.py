942807da6a83029ca2fd8c3f6ca1c545a8a75da0
Rawan Mohamed <rawanmohamed2023@u.northwestern.edu>
2022-05-04 22:24:25 -0500

    suggestions done

--- views/suggestions.py
+++ views/suggestions.py
@@ -12,8 +12,12 @@ class SuggestionsListEndpoint(Resource):
     def get(self):
         # suggestions should be any user with an ID that's not in this list:
         # print(get_authorized_user_ids(self.current_user))
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+        user_ids = get_authorized_user_ids(self.current_user)
 
+        suggestion = User.query.filter(User.id.notin_(user_ids)).limit(7).all()
+        suggestions_json = [suggestion.to_dict() for suggestion in suggestion]
+
+        return Response(json.dumps(suggestions_json), mimetype="application/json", status=200)
 
 def initialize_routes(api):
     api.add_resource(
