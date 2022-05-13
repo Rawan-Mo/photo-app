e9ba01e0782d18c1816217827a431b77ab07a659
Rawan Mohamed <rawanmohamed2023@u.northwestern.edu>
2022-05-13 14:59:41 -0500

    modal window + close button + view all comments button

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -209,7 +209,7 @@ const post2Html = post => {
 
                 ${post.comments.length > 1 ?  
                   `
-                  <p class="view-all blue"> View all ${post.comments.length} comments </p>
+                  <button class="view-all blue" onclick="openModal(event);"> View all ${post.comments.length} comments </button>
                   `
                   : ''
                 }
@@ -280,6 +280,34 @@ const displayPosts = () => {
         })
 };
 
+
+const modalElement = document.querySelector('.modal-bg');
+
+const openModal = ev => {
+    console.log('open!');
+    modalElement.classList.remove('hidden');
+    modalElement.setAttribute('aria-hidden', 'false');
+    document.querySelector('.close').focus();
+}
+
+const closeModal = ev => {
+    console.log('close!');
+    modalElement.classList.add('hidden');
+    modalElement.setAttribute('aria-hidden', 'false');
+    document.querySelector('.open').focus();
+};
+
+
+// function ensures that if the tabbing gets to the end of the 
+// modal, it will loop back up to the beginning of the modal:
+document.addEventListener('focus', function(event) {
+    console.log('focus');
+    if (modalElement.getAttribute('aria-hidden') === 'false' && !modalElement.contains(event.target)) {
+        console.log('back to top!');
+        event.stopPropagation();
+        document.querySelector('.close').focus();
+    }
+}, true);
 
 const initPage = () => {
     displayStories();
